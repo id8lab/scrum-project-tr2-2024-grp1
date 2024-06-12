b2d41107fd8063362f28567a18236b0721f4a2c4
austinliandro <austin.liandro@my.jcu.edu.au>
2024-06-12 12:35:58 +0800

    updated gamescreen to FullScreen Mode

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import random
 pygame.init()
 
 # Screen dimensions
-WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+WIDTH, HEIGHT = screen.get_size()
 pygame.display.set_caption("Space Shooter")
 
 # Colors
